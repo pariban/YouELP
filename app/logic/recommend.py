77f77113591880d1f9d2901799a3806732b04f35
Parika Bandyopadhyay <parikabandyopadhyay@gmail.com>
2017-10-01 23:12:17 +0000

    Fixing a bug on query case
    Needed to convert it to lowercase

--- app/logic/recommend.py
+++ app/logic/recommend.py
@@ -9,10 +9,11 @@ class Recommender():
             reader = csv.reader(infile, delimiter='\t')
             for row in reader:
                 self.correlation_map[row[0].lower()] = row[1].lower()
+            print >>sys.stderr, "loaded categories", len(self.correlation_map)
 
     def get_recommendations(self, query):
         # print >>sys.stderr, "getting recommendations for", query
-        tgt_category = self._get(query.query_string)
+        tgt_category = self._get(query.query_string.lower())
         # print >>sys.stderr, "tgt:", tgt_category
         if tgt_category:
             query.query_string = tgt_category
@@ -21,5 +22,5 @@ class Recommender():
 
     def _get(self, source_category):
         if source_category in self.correlation_map:
-            return self.correlation_map[source_category.lower()]
+            return self.correlation_map[source_category]
         return None
