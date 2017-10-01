3fb4ae605f331d53671a612bd5dd7d1c3036a69d
Ubuntu <ubuntu@ip-10-0-0-10.us-west-2.compute.internal>
2017-10-01 17:33:03 +0000

    Added connecting script to link with apache web server
    Also updated server.py accordingly

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 #!flask/bin/python
 from app import app
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+      app.run()
+# app.run(host='0.0.0.0', port=5000, debug=True)
+# app.run(host='0.0.0.0', port=5000, debug=True)
